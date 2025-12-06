6109fa97b1744a0eb16a817f4e47a54f3ac21c57
stringkarm <ccs.karmelieanncanasa@gmail.com>
2025-12-06 12:06:10 +0800

    Add project files.

---
+++ MAUIVerter/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MAUIVerter
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
